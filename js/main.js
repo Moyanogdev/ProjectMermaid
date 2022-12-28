c7db07a5683fed67c2339a14fd745b744009ce7d
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-12-28 14:42:48 -0300

    cambiosGenerales3

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// SCROLL REVEAL EFECTS
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 250,
+});
+
+sr.reveal(`.imgBanner`, {origin: 'left'})
+sr.reveal(`.textBanner`, {origin: 'right'})
+sr.reveal(`.efectoNails`, {origin: 'bottom'})
+sr.reveal(`.efectoTrabajos`)
+sr.reveal(`.efectoOtros`)
+sr.reveal(`.efectoDepilacion`, {origin: 'left'})
+sr.reveal(`.efectoLaminado`, {origin:'right'})
+sr.reveal(`.efectoContacto`)
+sr.reveal(`.efectoForm`, {origin: 'left'})
+sr.reveal(`.efectoMap`, {origin: 'right'})
